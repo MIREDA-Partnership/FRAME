fdaa0c126bc8e3ea24e4e71173fced0c955ee315
sdurbaba <stevo.durbaba@kcl.ac.uk>
2026-06-22 12:21:25 +0100

    update v3

--- scripts/test_cohort_section/FRAME_cohort_selection_test_v3.R
+++ scripts/test_cohort_section/FRAME_cohort_selection_test_v3.R
@@ -57,11 +57,11 @@ out_folder = "B:/BRC_Elixir/Durbaba- MIREDA/test OMOP/FRAME OMOP/version 1/"
 
 #Set file names#
 
-script_version = 2
+script_version = 3
 
 
 #main data file
-out_file = "FRAME mothers data for cohort section v1.csv"
+out_file = paste0("FRAME mothers data for cohort section v",as.character(script_version),".csv")
 
 # additional data files
 
@@ -91,9 +91,23 @@ nifedipine_concept_id <- 1318853
 
 drug_list <- c(labetalol_concept_id,nifedipine_concept_id)
 
-
 # Need hypertensive_disorder
 hypertensive_disorder_concept_id <- 316866
+
+
+# exclusion observations
+#nifedipine_allergy_concept_id  <- 4166010 #Allergy to nifedipine
+#labetalol_allergy_concept_id  <- 4169378	#Allergy to labetalol
+
+#exclude_obs_list<-c(nifedipine_allergy_concept_id,labetalol_allergy_concept_id)
+
+
+# exclusion conditions
+
+#maternal_tuberculosis_concept_id <- 434416	#Maternal tuberculosis during pregnancy, childbirth and the puerperium (exclude due to issue with nifedipine)
+
+#exclude_co_list<-c(maternal_tuberculosis_concept_id )
+
 
 
 # relationships
@@ -144,6 +158,17 @@ hypertensive_descendants_list <- c( hypertensive_disorder_concept_id,  vocab_df_
 
 # hypertensive_disorder_df
 hypertensive_descendants_df <-  vocab_df_list[["CONCEPT"]] %>% filter( concept_id %in% hypertensive_descendants_list )
+
+
+# labetalol descendants
+labetalol_descendants_list <- c( labetalol_concept_id , vocab_df_list[["CONCEPT_ANCESTOR"]] %>% filter(ancestor_concept_id  == labetalol_concept_id ) %>%
+                                   select(descendant_concept_id) %>% unlist(use.names = FALSE))
+
+# nifedipine_descendants
+nifedipine_descendants_list <- c( nifedipine_concept_id , vocab_df_list[["CONCEPT_ANCESTOR"]] %>% filter(ancestor_concept_id  == nifedipine_concept_id ) %>%
+                                   select(descendant_concept_id) %>% unlist(use.names = FALSE))
+
+
 
 
 
@@ -218,7 +243,20 @@ str(preg_mum_df)
 # calculate week 11 from start date of pregnancy
 #preg_df$preg_week_11_date <- ymd(preg_df$condition_start_date) + weeks(11)
 
-# has drugs recorded labetalol and nifedipine 
+
+#labetalol_concept_id
+#nifedipine_concept_id
+
+
+# has drugs recorded labetalol
+has_labetalol_df <-  omop_df_list[["drug_exposure"]] %>% filter(drug_concept_id %in% labetalol_descendants_list )
+
+# has drugs recorded labetalol or nifedipine 
+has_nifedipine_df <-  omop_df_list[["drug_exposure"]] %>% filter(drug_concept_id %in% nifedipine_descendants_list )
+
+
+
+# has drugs recorded labetalol or nifedipine 
 has_drugs_df <-  omop_df_list[["drug_exposure"]] %>% filter(drug_concept_id %in% drug_list_descendants_list )
 
 #eyeball
@@ -247,6 +285,22 @@ rf_has_hyp_df <- omop_df_list[["fact_relationship"]] %>%
   filter(relationship_concept_id == crt_relationship_id ) %>%
   filter( fact_id_2 %in% c( has_hyp_df %>% select(hyp_condition_occurrence_id)  %>% unlist(use.names = FALSE )  ) )
 
+# get the fact relationship to link pregnancy as condition relevant to labetalol
+rf_labetalol_df <- omop_df_list[["fact_relationship"]] %>% 
+  filter(domain_concept_id_1 == co_domain_concept_id ) %>%  
+  filter(domain_concept_id_2 == drug_domain_concept_id ) %>%
+  filter(relationship_concept_id == crt_relationship_id ) %>%
+  filter( fact_id_2 %in% c(has_labetalol_df %>% select(drug_exposure_id)  %>% unlist(use.names = FALSE)) ) 
+
+
+# get the fact relationship to link pregnancy as condition relevant to nifedipine 
+rf_nifedipine_df <- omop_df_list[["fact_relationship"]] %>% 
+  filter(domain_concept_id_1 == co_domain_concept_id ) %>%  
+  filter(domain_concept_id_2 == drug_domain_concept_id ) %>%
+  filter(relationship_concept_id == crt_relationship_id ) %>%
+  filter( fact_id_2 %in% c(has_nifedipine_df %>% select(drug_exposure_id)  %>% unlist(use.names = FALSE)) ) 
+
+
 
 # get the fact relationship to link pregnancy as condition relevant to labetalol and nifedipine 
 rf_has_drugs_df <- omop_df_list[["fact_relationship"]] %>% 
@@ -257,13 +311,14 @@ rf_has_drugs_df <- omop_df_list[["fact_relationship"]] %>%
 
 
 
+
 # megre preg_mum_df to preg_df to calculate age and excluded any not aged 18+ at conception
 preg_link_df <- preg_df %>% inner_join(preg_mum_df ,by=join_by( person_id))
 
+#eyeball preg_link_df
 str(preg_link_df)
 
 # remove pergancies under 18
-
 preg_18_link_df <- preg_link_df %>%
   mutate(
     birthday_18 = ymd(birth_datetime) + years(18)
@@ -278,6 +333,9 @@ str(preg_18_link_df)
 nrow(preg_link_df)- nrow(preg_18_link_df)
 
 
+#add condition exclution
+
+
 # add a column for  hypertension in pregnancy 
 
 # merge the pregnancy condition with fact relationship link to merge with hyp
@@ -290,7 +348,28 @@ preg_link_has_hyp_df <- preg_18_link_df %>%
 # eyeball
 str(preg_link_has_hyp_df)
 
+# merge the pregnancy condition with fact relationship link to merge with nifedipine  and then merge with person table for DOB
+preg_link_nifedipine_df <- preg_18_link_df %>% 
+  inner_join(rf_nifedipine_df,by=join_by(preg_condition_occurrence_id==fact_id_1)) %>%
+  inner_join(has_nifedipine_df %>% select(drug_exposure_id ,drug_concept_id,drug_exposure_start_date ),by=join_by(fact_id_2 ==drug_exposure_id )) %>%
+  mutate(nifedipine_in_preg = if_else(  (drug_exposure_start_date >=  preg_condition_start_date )  &   (drug_exposure_start_date <=  preg_condition_end_date),TRUE,FALSE )) %>%
+  mutate(nifedipine_before_preg = if_else(  drug_exposure_start_date <=  preg_condition_start_date ,TRUE,FALSE ))
 
+
+# eyeball
+str(preg_link_nifedipine_df)
+
+
+# merge the pregnancy condition with fact relationship link to merge with labetalol  and then merge with person table for DOB
+preg_link_labetalol_df <- preg_18_link_df %>% 
+  inner_join(rf_labetalol_df,by=join_by(preg_condition_occurrence_id==fact_id_1)) %>%
+  inner_join(has_labetalol_df %>% select(drug_exposure_id ,drug_concept_id,drug_exposure_start_date ),by=join_by(fact_id_2 ==drug_exposure_id )) %>%
+  mutate(labetalol_in_preg = if_else(  (drug_exposure_start_date >=  preg_condition_start_date )  &   (drug_exposure_start_date <=  preg_condition_end_date),TRUE,FALSE )) %>%
+  mutate(labetalol_before_preg = if_else(  drug_exposure_start_date <=  preg_condition_start_date ,TRUE,FALSE ))
+
+
+# eyeball
+str(preg_link_labetalol_df)
 
 
 # merge the pregnancy condition with fact relationship link to merge with labetalol and nifedipine  and then merge with person table for DOB
@@ -299,6 +378,7 @@ preg_link_has_drugs_df <- preg_18_link_df %>%
   inner_join(has_drugs_df %>% select(drug_exposure_id ,drug_concept_id,drug_exposure_start_date ),by=join_by(fact_id_2 ==drug_exposure_id )) %>%
   mutate(drugs_in_preg = if_else(  (drug_exposure_start_date >=  preg_condition_start_date )  &   (drug_exposure_start_date <=  preg_condition_end_date),TRUE,FALSE )) %>%
   mutate(drugs_before_preg = if_else(  drug_exposure_start_date <=  preg_condition_start_date ,TRUE,FALSE ))
+
 
 # eyeball
 str(preg_link_has_drugs_df)
@@ -310,6 +390,14 @@ preg_18_reporting_df <- preg_18_link_df %>%
   mutate(hyp_recorded = if_else(preg_condition_occurrence_id %in%  preg_link_has_hyp_df$preg_condition_occurrence_id, TRUE, FALSE ) ) %>%
   mutate(hyp_in_preg = if_else(preg_condition_occurrence_id %in%  preg_link_has_hyp_df$preg_condition_occurrence_id[preg_link_has_hyp_df$hyp_in_preg == T], TRUE, FALSE ) ) %>%
   mutate(hyp_before_preg = if_else(preg_condition_occurrence_id %in%  preg_link_has_hyp_df$preg_condition_occurrence_id[preg_link_has_hyp_df$hyp_before_preg == T], TRUE, FALSE ) ) %>%
+  mutate(l_recorded = if_else(preg_condition_occurrence_id %in%  preg_link_labetalol_df$preg_condition_occurrence_id, TRUE, FALSE ) ) %>%
+  mutate(l_in_preg = if_else(preg_condition_occurrence_id %in%  preg_link_labetalol_df$preg_condition_occurrence_id[preg_link_labetalol_df$labetalol_in_preg == T], TRUE, FALSE ) ) %>%
+  mutate(l_before_preg = if_else(preg_condition_occurrence_id %in%  preg_link_labetalol_df$preg_condition_occurrence_id[preg_link_labetalol_df$labetalol_before_preg == T], TRUE, FALSE ) ) %>%
+  left_join(preg_link_labetalol_df %>% group_by(preg_condition_occurrence_id) %>% summarise(l_initiated_date=min(drug_exposure_start_date)),by=join_by(preg_condition_occurrence_id==preg_condition_occurrence_id)) %>% 
+  mutate(n_recorded = if_else(preg_condition_occurrence_id %in%  preg_link_nifedipine_df$preg_condition_occurrence_id, TRUE, FALSE ) ) %>%
+  mutate(n_in_preg = if_else(preg_condition_occurrence_id %in%  preg_link_nifedipine_df$preg_condition_occurrence_id[preg_link_nifedipine_df$nifedipine_in_preg == T], TRUE, FALSE ) ) %>%
+  mutate(n_before_preg = if_else(preg_condition_occurrence_id %in%  preg_link_nifedipine_df$preg_condition_occurrence_id[preg_link_nifedipine_df$nifedipine_before_preg == T], TRUE, FALSE ) ) %>%
+  left_join(preg_link_nifedipine_df %>% group_by(preg_condition_occurrence_id) %>% summarise(n_initiated_date=min(drug_exposure_start_date)),by=join_by(preg_condition_occurrence_id==preg_condition_occurrence_id)) %>% 
   mutate(l_n_drugs_recorded = if_else(preg_condition_occurrence_id %in%  preg_link_has_drugs_df$preg_condition_occurrence_id, TRUE, FALSE ) ) %>%
   mutate(l_n_drugs_in_preg = if_else(preg_condition_occurrence_id %in%  preg_link_has_drugs_df$preg_condition_occurrence_id[preg_link_has_drugs_df$drugs_in_preg == T], TRUE, FALSE ) ) %>%
   mutate(l_n_drugs_before_preg = if_else(preg_condition_occurrence_id %in%  preg_link_has_drugs_df$preg_condition_occurrence_id[preg_link_has_drugs_df$drugs_before_preg == T], TRUE, FALSE ) ) %>%
@@ -328,6 +416,8 @@ table(preg_18_reporting_df$l_n_drugs_recorded)
 table(preg_18_reporting_df$l_n_drugs_in_preg )
 table(preg_18_reporting_df$l_n_drugs_before_preg)
 table(preg_18_reporting_df$frame_cohort)
+table(preg_18_reporting_df$n_in_preg )
+table(preg_18_reporting_df$l_in_preg )
 
 
 # check names
@@ -368,12 +458,16 @@ res_df <- res_df %>% bind_rows(data.frame(item='number of labetalol or nifedipin
 res_df <- res_df %>% bind_rows(data.frame(item='number of pregnancies aged 18+ with hypertensive disorder code ever recorded ',item_value =nrow(preg_18_reporting_df %>% filter(hyp_recorded ==TRUE))))
 res_df <- res_df %>% bind_rows(data.frame(item='number of pregnancies aged 18+ with hypertensive disorder code recorded during pregnancy',item_value =nrow(preg_18_reporting_df %>% filter(hyp_in_preg==TRUE))))
 res_df <- res_df %>% bind_rows(data.frame(item='number of pregnancies aged 18+ with hypertensive disorder code recorded before pregnancy',item_value =nrow(preg_18_reporting_df %>% filter(hyp_before_preg==TRUE))))
-res_df <- res_df %>% bind_rows(data.frame(item='number of pregnancies aged 18+ with labetalol or nifedipiner code ever recorded ',item_value =nrow(preg_18_reporting_df %>% filter(l_n_drugs_recorded ==TRUE))))
-res_df <- res_df %>% bind_rows(data.frame(item='number of pregnancies aged 18+ with labetalol or nifedipiner code recorded during pregnancy',item_value =nrow(preg_18_reporting_df %>% filter(l_n_drugs_in_preg==TRUE))))
-res_df <- res_df %>% bind_rows(data.frame(item='number of pregnancies aged 18+ with labetalol or nifedipiner code recorded before pregnancy',item_value =nrow(preg_18_reporting_df %>% filter(l_n_drugs_before_preg==TRUE))))
-res_df <- res_df %>% bind_rows(data.frame(item='number of pregnancies aged 18+ with with hypertensive disorder code ever recorded and  labetalol or nifedipiner code recorded during pregnancy',item_value =nrow(preg_18_reporting_df %>% filter(frame_cohort==TRUE))))
-
-
+res_df <- res_df %>% bind_rows(data.frame(item='number of pregnancies aged 18+ with labetalol code ever recorded ',item_value =nrow(preg_18_reporting_df %>% filter(l_recorded ==TRUE))))
+res_df <- res_df %>% bind_rows(data.frame(item='number of pregnancies aged 18+ with labetalol code recorded during pregnancy',item_value =nrow(preg_18_reporting_df %>% filter(l_in_preg==TRUE))))
+res_df <- res_df %>% bind_rows(data.frame(item='number of pregnancies aged 18+ with labetalol code recorded before pregnancy',item_value =nrow(preg_18_reporting_df %>% filter(l_before_preg==TRUE))))
+res_df <- res_df %>% bind_rows(data.frame(item='number of pregnancies aged 18+ with nifedipine code ever recorded ',item_value =nrow(preg_18_reporting_df %>% filter(n_recorded ==TRUE))))
+res_df <- res_df %>% bind_rows(data.frame(item='number of pregnancies aged 18+ with nifedipine code recorded during pregnancy',item_value =nrow(preg_18_reporting_df %>% filter(n_in_preg==TRUE))))
+res_df <- res_df %>% bind_rows(data.frame(item='number of pregnancies aged 18+ with nifedipine code recorded before pregnancy',item_value =nrow(preg_18_reporting_df %>% filter(n_before_preg==TRUE))))
+res_df <- res_df %>% bind_rows(data.frame(item='number of pregnancies aged 18+ with labetalol or nifedipine code ever recorded ',item_value =nrow(preg_18_reporting_df %>% filter(l_n_drugs_recorded ==TRUE))))
+res_df <- res_df %>% bind_rows(data.frame(item='number of pregnancies aged 18+ with labetalol or nifedipine code recorded during pregnancy',item_value =nrow(preg_18_reporting_df %>% filter(l_n_drugs_in_preg==TRUE))))
+res_df <- res_df %>% bind_rows(data.frame(item='number of pregnancies aged 18+ with labetalol or nifedipine code recorded before pregnancy',item_value =nrow(preg_18_reporting_df %>% filter(l_n_drugs_before_preg==TRUE))))
+res_df <- res_df %>% bind_rows(data.frame(item='number of pregnancies aged 18+ with with hypertensive disorder code ever recorded and  labetalol or nifedipine code recorded during pregnancy',item_value =nrow(preg_18_reporting_df %>% filter(frame_cohort==TRUE))))
 
 
 # create  column_classes_list 
